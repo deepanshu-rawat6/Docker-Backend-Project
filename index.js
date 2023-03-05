8a14cb8eb75327aad09251ec311ba511d4b8742e
deepanshu-rawat6 <deepanshu.dr10@gmail,com>
2023-03-05 13:17:28 +0530

    adding docker-compse;updating project files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("<h2>Hello there, I'm Deepanshu!!!</h2>");
+    res.send("<h2>Hello there, I'm Deepanshu, trying to build this node-docker-express app</h2>");
 });
 
 const port = process.env.PORT || 3000;
