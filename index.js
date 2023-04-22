28355e66e4d18399ef13eb5f04c391c42b1745f1
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2023-04-22 18:39:56 +0530

    connecting mongo and node

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
+
+// Because of DNS, we can simply re write the IP to "mongo"
+mongoose.connect(
+    'mongodb://deepanshu:mypassword@mongo:27017/?authSource=admin') // we have specified "mongo", because even if the IP address changes is it linked with the service "mongo"
+    .then(() => console.log("Successfully connected to DB"))
+    .catch((e) => console.log(e)
+);
 
 app.get("/", (req, res) => {
     res.send("<h2>Hello there, I'm Deepanshu, trying to build this node-docker-express app, using docker-compose and distributing the workflow into dev and prod env</h2>");
@@ -8,4 +16,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log('Listening on port ${port}'))
+app.listen(port, () => console.log('Listening on port ${port}'));
